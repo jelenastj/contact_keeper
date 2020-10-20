337fc2aa3bb4eed75b1c027d866c58aef217374c
jelenastj <jelena.stjepanovic@gmail.com>
2020-10-19 17:50:06 -0700

    edit and update contact action

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -63,6 +63,9 @@ const ContactState = props => {
   }
 
   //Update Contact
+  const updateContact = contact => {
+    dispatch({type: UPDATE_CONTACT, payload: contact})
+  }
 
   //Filter Contacts
 
@@ -76,11 +79,12 @@ const ContactState = props => {
         addContact,
         deleteContact,
         setCurrent,
-        clearCurrent
+        clearCurrent,
+        updateContact
       }}>
       { props.children}
     </ContactContext.Provider>
   )
 };
 
-export default ContactState;
+export default ContactState
