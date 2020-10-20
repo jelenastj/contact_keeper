b126ce108942cc1f2877778db30eb9e53f8992ca
jelenastj <jelena.stjepanovic@gmail.com>
2020-10-19 18:51:32 -0700

    contact filter form and state, add and delete animation

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,16 +1,49 @@
 import React, { Fragment, useContext, useEffect } from 'react';
-import ContactItem from './ContactItem'
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import ContactItem from './ContactItem';
+// import Spinner from '../layout/Spinner';
 import ContactContext from '../../context/contact/contactContext';
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
-  const { contacts } = contactContext;
+  const { contacts, filtered } = contactContext;
+
+  // useEffect(() => {
+  //   getContacts();
+  //   // eslint-disable-next-line
+  // }, []);
+
+  if (contacts.length === 0 ) {
+    return <h4>Please add a contact</h4>;
+  }
 
   return (
     <Fragment>
-        {contacts.map(contact => (<ContactItem contact={contact} key={contact.id}/>))}
+     
+        <TransitionGroup>
+          {filtered !== null
+            ? filtered.map(contact => (
+                <CSSTransition
+                  key={contact._id}
+                  timeout={500}
+                  classNames='item'
+                >
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              ))
+            : contacts.map(contact => (
+                <CSSTransition
+                  key={contact._id}
+                  timeout={500}
+                  classNames='item'
+                >
+                  <ContactItem contact={contact} />
+                </CSSTransition>
+              ))}
+        </TransitionGroup>
     </Fragment>
-  )
-}
-export default Contacts
+  );
+};
+
+export default Contacts;
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import {v4 as uuid} from "uuid"; 
+import { v4 as uuid } from "uuid";
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
@@ -38,49 +38,58 @@ const ContactState = props => {
       }
     ],
     current: null,
+    filtered: null,
   };
+
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   //Add Contact
   const addContact = contact => {
     contact.id = uuid();
-    dispatch({type: ADD_CONTACT, payload: contact})
+    dispatch({ type: ADD_CONTACT, payload: contact })
   }
 
   //Delete Contact
   const deleteContact = id => {
-    dispatch({type: DELETE_CONTACT, payload: id})
+    dispatch({ type: DELETE_CONTACT, payload: id })
   }
 
   //Set current contact 
   const setCurrent = contact => {
-    dispatch({type: SET_CURRENT, payload: contact})
+    dispatch({ type: SET_CURRENT, payload: contact })
   }
 
   //Clear Current Contact
   const clearCurrent = () => {
-    dispatch({type: CLEAR_CURRENT})
+    dispatch({ type: CLEAR_CURRENT })
   }
 
   //Update Contact
   const updateContact = contact => {
-    dispatch({type: UPDATE_CONTACT, payload: contact})
+    dispatch({ type: UPDATE_CONTACT, payload: contact })
   }
 
   //Filter Contacts
+  const filterContact = text => {
+    dispatch({ type: FILTER_CONTACTS, payload: text })}
 
   //Clear Filter
+ const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER })}
 
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
+        filtered: state.filtered,
         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
-        updateContact
+        updateContact,
+        filterContact,
+        clearFilter
       }}>
       { props.children}
     </ContactContext.Provider>
