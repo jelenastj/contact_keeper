a0b39a4773dd90c7908c1b8c4aafc15b93f3d691
jelenastj <jelena.stjepanovic@gmail.com>
2020-10-20 19:08:36 -0700

    add contact

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -5,7 +5,8 @@ import {
   CLEAR_CURRENT,
   UPDATE_CONTACT,
   FILTER_CONTACTS,
-  CLEAR_FILTER
+  CLEAR_FILTER,
+  CONTACT_ERROR,
 } from '../types';
 
 export default (state, action) => {
@@ -18,7 +19,7 @@ export default (state, action) => {
     case DELETE_CONTACT:
       return {
         ...state,
-        contacts: state.contacts.filter(contact => contact.id !== action.payload)
+        contacts: state.contacts.filter(contact => contact._id !== action.payload)
       }
     case SET_CURRENT:
       return {
@@ -33,7 +34,7 @@ export default (state, action) => {
     case UPDATE_CONTACT:
       return {
         ...state,
-        contacts: state.contacts.map(contact => contact.id === action.payload.id ? action.payload : contact)
+        contacts: state.contacts.map(contact => contact._id === action.payload.id ? action.payload : contact)
       }
     case FILTER_CONTACTS:
       return {
@@ -43,11 +44,16 @@ export default (state, action) => {
           return contact.name.match(regex) || contact.email.match(regex);
         })
       };
-      case CLEAR_FILTER:
-        return {
-          ...state,
-          filtered: null
-        }
+    case CLEAR_FILTER:
+      return {
+        ...state,
+        filtered: null
+      }
+    case CONTACT_ERROR:
+      return {
+        ...state,
+        error: action.payload
+      };
     default:
       return state;
   }
